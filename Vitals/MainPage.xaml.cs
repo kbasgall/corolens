683cbb01306807c38eb84c3c3b41ba2145501baa
Kristen Basgall <35284460+kbasgall@users.noreply.github.com>
2019-10-19 20:43:23 -0400

    update ui as we parse data

--- Vitals/MainPage.xaml.cs
+++ Vitals/MainPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,7 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
+using System.Xml;
+using System.Threading;
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace Vitals
@@ -26,13 +27,71 @@ namespace Vitals
     }
     public sealed partial class MainPage : Page
     {
+        String systolic_blood_pressure_val = "--";
+        String diastolic_blood_pressure_val = "--";
+        String heartrate_val = "--";
+        String oxygen_val = "--";
+        String temperature_val = "--";
+
         public MainPage()
         {
             this.InitializeComponent();
-            blood_pressure.DataContext = new TextboxText() { Textdata = "100/80" };
-            heartrate.DataContext = new TextboxText() { Textdata = "60" };
-            oxygen_level.DataContext = new TextboxText() { Textdata = "80" };
-            temperature.DataContext = new TextboxText() { Textdata = "98.6" };
+            blood_pressure.DataContext = new TextboxText() { Textdata = systolic_blood_pressure_val + "/" + diastolic_blood_pressure_val };
+            heartrate.DataContext = new TextboxText() { Textdata = heartrate_val };
+            oxygen_level.DataContext = new TextboxText() { Textdata = oxygen_val };
+            temperature.DataContext = new TextboxText() { Textdata = temperature_val };
+
+            ParseDataFromSocket();
+        }
+
+        public void ParseDataFromSocket()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(@"originalv3Message.xml");
+
+            XmlNamespaceManager mgr = new XmlNamespaceManager(doc.NameTable);
+
+            mgr.AddNamespace("hl7", "urn:hl7-org:v3");
+
+            XmlNodeList vital_values = doc.DocumentElement.SelectNodes("//hl7:POLB_IN224200UV01/hl7:controlActProcess/hl7:subject/hl7:observationBattery/hl7:component1/hl7:observationEvent/hl7:value", mgr);
+            XmlNodeList display_names = doc.DocumentElement.SelectNodes("//hl7:POLB_IN224200UV01/hl7:controlActProcess/hl7:subject/hl7:observationBattery/hl7:component1/hl7:observationEvent/hl7:code", mgr);
+
+            for (int i = 0; i < vital_values.Count; ++i)
+            {
+                String curr_val = vital_values[i].Attributes["value"].Value;
+                // Update individual values
+                if (display_names[i].Attributes["displayName"].Value == "Body temperature")
+                {
+                    double fahrenheit = ((Double.Parse(curr_val) * 9) / 5) + 32;
+                    String result = string.Format("{0:0.0}", Math.Truncate(fahrenheit * 10) / 10);
+                    temperature_val = result;
+                    temperature.DataContext = new TextboxText() { Textdata = temperature_val };
+                }
+                else if (display_names[i].Attributes["displayName"].Value == "Systolic blood pressure")
+                {
+                    systolic_blood_pressure_val = curr_val;
+                    blood_pressure.DataContext = new TextboxText() { Textdata = systolic_blood_pressure_val + "/" + diastolic_blood_pressure_val };
+                }
+                else if(display_names[i].Attributes["displayName"].Value == "Diastolic blood pressure")
+                {
+                    diastolic_blood_pressure_val = curr_val;
+                    blood_pressure.DataContext = new TextboxText() { Textdata = systolic_blood_pressure_val + "/" + diastolic_blood_pressure_val };
+                }
+                else if (display_names[i].Attributes["displayName"].Value == "Mean blood pressure")
+                {
+
+                }
+                else if (display_names[i].Attributes["displayName"].Value == "Pulse rate")
+                {
+                    heartrate_val = curr_val;
+                    heartrate.DataContext = new TextboxText() { Textdata = heartrate_val };
+                }
+                else if (display_names[i].Attributes["displayName"].Value == "SpO2")
+                {
+                    oxygen_val = curr_val;
+                    oxygen_level.DataContext = new TextboxText() { Textdata = oxygen_val };
+                }
+            }
         }
     }
 }
